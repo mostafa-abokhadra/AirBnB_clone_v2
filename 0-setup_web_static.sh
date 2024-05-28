56975a65f73e308366001dc6a88b5ca4e694defd
mostafa-abokhadra <mvxizk@gmail.com>
2024-05-28 22:49:26 +0300

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ if [ -L /data/web_static/current ]; then
         sudo rm /data/web_static/current
 fi
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '$ a \
 \
 server {\
