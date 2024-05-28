6da8cbedf25f5fd485ba038af806a7c79d23f974
mostafa-abokhadra <mvxizk@gmail.com>
2024-05-28 23:12:03 +0300

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,22 +2,10 @@
 # deploying webstatic
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "almost there :)" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '$ a \
-\
-server {\
-    listen 80;\
-    server_name bukhadra.tech;\
-\
-    location /hbnb_static {\
-        alias /data/web_static/current/;\
-    }\
-\
-}\
-' /etc/nginx/sites-available/default
+sudo sed -i '72i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
