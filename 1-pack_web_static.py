54aa388e0cc35b1cf74438ed3d6b3fbc2872b316
mostafa-abokhadra <mvxizk@gmail.com>
2024-05-28 23:49:22 +0300

    done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# deploying
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+@task
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
