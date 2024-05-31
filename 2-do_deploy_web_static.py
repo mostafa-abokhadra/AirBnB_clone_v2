4eaf1e7b16f5ce24dad0aa8a14274ffdfe78ad94
mostafa-abokhadra <mvxizk@gmail.com>
2024-05-31 20:00:52 +0300

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,12 +21,14 @@ def do_deploy(archive_path):
             file_name, file_name.split('.')[0]))
 
         run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(
+        run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(
             file_name.split('.')[0], file_name.split('.')[0]))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
             file_name.split('.')[0]))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(
+        run("ln -s /data/web_static/releases/{} \
+                /data/web_static/current".format(
             file_name.split('.')[0]))
         print("New version deployed!")
         return True
