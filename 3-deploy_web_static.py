549f8f81905ac5146c7b8b1766a56afa0c286749
mostafa-abokhadra <mvxizk@gmail.com>
2024-05-31 20:44:55 +0300

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-# full deploy
+"""full deploy"""
 
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 do_pack = __import__('1-pack_web_static').do_pack
 
+
 def deploy():
+    """full deploy"""
     path = do_pack()
-    if path == None:
+    if path is None:
         return False
     do_deploy(path)
