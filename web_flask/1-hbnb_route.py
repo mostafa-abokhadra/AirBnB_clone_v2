09cdb6b3022ee8ed569556032f150e3acbae80ca
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-06 03:28:13 +0300

    ok

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""starting with flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
