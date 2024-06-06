ddfe7a80d26799c186c3adfd5d544474497e11aa
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-06 03:26:18 +0300

    ok

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+# returning hello
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
