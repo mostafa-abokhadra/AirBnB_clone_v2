b271b8f7205ea586e1e49bcf37774e80a5ae75f9
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-06 03:46:54 +0300

    ok

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def fun(text):
-    return "c {}".format(text.replace('_', ' '))
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
