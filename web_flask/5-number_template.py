fcebccab47a0550b7a1207b9889eafd31770f423
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-06 04:10:46 +0300

    ok

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -31,7 +31,7 @@ def num(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>',strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
     return render_template('5-number.html', number=n)
 
