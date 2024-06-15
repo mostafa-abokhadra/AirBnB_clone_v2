c3e0ec7c6fa95bfd0372b796fa583d70d800f90d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-15 16:57:31 +0300

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """full deploy"""
 
+import os.path
+from fabric.api import put, env, run, local
+from datetime import datetime
+
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 do_pack = __import__('1-pack_web_static').do_pack
 
+env.hosts = ["ubuntu@54.90.62.95", "ubuntu@54.90.37.231"]
+env.user = "ubuntu"
 
 def deploy():
     """full deploy"""
