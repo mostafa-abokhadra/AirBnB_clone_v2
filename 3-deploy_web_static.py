48a633bddc45f0c96e9d1516b51feff01f89310d
mostafa-abokhadra <mvxizk@gmail.com>
2024-06-15 17:09:34 +0300

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,54 @@
 #!/usr/bin/python3
 """full deploy"""
 
-import os.path
-from fabric.api import put, env, run, local
+from fabric.api import local, put, env, run
 from datetime import datetime
-
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
-do_pack = __import__('1-pack_web_static').do_pack
+import os.path
 
 env.hosts = ["ubuntu@54.90.62.95", "ubuntu@54.90.37.231"]
 env.user = "ubuntu"
+
+
+def do_pack():
+    """making archeive"""
+    try:
+        local('mkdir -p versions')
+        archive_path = 'versions/web_static_{}.tgz'.format(
+                    datetime.now().strftime('%Y%m%d%H%M%S'))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(
+                archive_path, os.path.getsize(archive_path)))
+        return archive_path
+    except Exception:
+        return No
+
+
+def do_deploy(archive_path):
+    """deploying to server
+    """
+    if os.path.exists(archive_path):
+        file_name = archive_path.split('/')[-1]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}".format(
+            file_name.split('.')[0]))
+        run("tar -xf /tmp/{}  -C /data/web_static/releases/{}".format(
+            file_name, file_name.split('.')[0]))
+
+        run("rm /tmp/{}".format(file_name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(
+            file_name.split('.')[0], file_name.split('.')[0]))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            file_name.split('.')[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} \
+                /data/web_static/current".format(
+            file_name.split('.')[0]))
+        print("New version deployed!")
+        return True
+    else:
+        return False
+
 
 def deploy():
     """full deploy"""
